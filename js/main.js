54c1049750bd6caa606e2a4901164f643e4607b7
eugeneart <artsiukhevicheugene@gmail.com>
2016-01-07 10:01:03 +0000

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $( document ).ready(function() {
-    // Navigation
-    $(".submenu").find("a").click(function () {
+    // menu-toggle
+    $(".submenu").find(".submenu__item").click(function () {
         $(this).next(".submenu-list").slideToggle("slow");
         $(this).toggleClass("submenu__open");
     });
+    //submenu-toggle
     $(".submenu-list__item").click(function(){
-        
+        $(this).find(".list-inside").slideToggle("slow")
+        $(this).find(".list-item").toggleClass("list-item__open");
     });
+    $(".navbar-toggle").click(function(){
+        $(this).toggleClass("navbar-toggle__open")
+    })
 });
