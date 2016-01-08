d1de8bb980440d8b3c81e9542f23991f7d0d637c
eugeneart <artsiukhevicheugene@gmail.com>
2016-01-08 19:15:47 +0000

    slider alpha version

--- js/main.js
+++ js/main.js
@@ -12,4 +12,42 @@ $( document ).ready(function() {
     $(".navbar-toggle").click(function(){
         $(this).toggleClass("navbar-toggle__open");
     })
+    
+    // SLIDER
+        var contentwidth = $(".slider").width();
+        var totalcontent = $(".content").size();
+        var allcontentwidth = contentwidth * totalcontent;
+        $(".contentslider").css({'width' : allcontentwidth});
+        // $(".slider-wrapper").removeClass("bgactive")
+        // $(".slider-wrapper").addClass("bgactive")
+        rotate = function(){
+            var slideid = $active.attr("rel") - 1;
+            var slidedistance = slideid * contentwidth;
+            $(".contentnav a").removeClass("active");
+            $active.addClass('active');
+            $(".contentslider").animate({
+                    left: -slidedistance
+                }, 500 );
+        }; 
+        
+        rotation = function(){
+            play = setInterval(function(){
+                $active = $(".contentnav a.active").next();
+                if ( $active.length === 0) {
+                    $active = $('.contentnav a:first');
+                }
+                rotate();
+                }, 5000);
+        };
+        
+        rotation();
+        
+        $(".contentnav a").click(function() {
+            $active = $(this);
+            clearInterval(play);
+            rotate();
+            rotation();
+            return false;
+        });
+        // });
 });
